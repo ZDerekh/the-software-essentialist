a12e5ee99244eda40e2d872121e203e3a58c2b53
zderekh <zahariya.derekh@gmail.com>
2024-12-22 16:06:34 +0100

    refactor: improved the test with parametrisation of tests

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,36 +1,53 @@
 import calculateStats from "./index";
 
 describe('stats calculator', () => {
-    it('should know that the minimum value in [2, 4, 21, -8, 53, 40] sequence is -8', () => {
-        const sequence = [2, 4, 21, -8, 53, 40];
 
+    it.each([
+        [[2, 4, 21, -8, 53, 40], -8],
+        [[1, 2, 3, 4, 5], 1],
+        [[-10, -20, -30, -40, -50], -50],
+        [[0, 0, 0, 0, 0], 0],
+        [[100, 200, 300, 400, 500], 100],
+    ])('should know that the minimum value in %s sequence is %s', (sequence, expected) => {
         const output = calculateStats(sequence);
 
-        expect(output.min).toBe(-8)
+        expect(output.min).toBe(expected)
     })
 
-    it('should know that the maximum value in [2, 4, 21, -8, 53, 40] sequence is 53', () => {
-        const sequence = [2, 4, 21, -8, 53, 40];
-
+    it.each([
+        [[2, 4, 21, -8, 53, 40], 53],
+        [[1, 2, 3, 4, 5], 5],
+        [[-10, -20, -30, -40, -50], -10],
+        [[0, 0, 0, 0, 0], 0],
+        [[100, 200, 300, 400, 500], 500],
+    ])('should know that the maximum value in %s sequence is %s', (sequence, expected) => {
         const output = calculateStats(sequence);
 
-        expect(output.max).toBe(53)
+        expect(output.max).toBe(expected)
     })
 
-    it('should know that the average value in [2, 4, 21, -8, 53, 40] sequence is 18.666666666667', () => {
-        const sequence = [2, 4, 21, -8, 53, 40];
-
+    it.each([
+        [[2, 4, 21, -8, 53, 40], 18.666666666667],
+        [[1, 2, 3, 4, 5], 3],
+        [[-10, -20, -30, -40, -50], -30],
+        [[0, 0, 0, 0, 0], 0],
+        [[100, 200, 300, 400, 500], 300],
+    ])('should know that the average value in %s sequence is %s', (sequence, expected) => {
         const output = calculateStats(sequence);
 
-        expect(output.avg).toBeCloseTo(18.666666666667)
+        expect(output.avg).toBeCloseTo(expected)
     })
 
-    it('should know that the count of values in [2, 4, 21, -8, 53, 40] sequence is 6', () => {
-        const sequence = [2, 4, 21, -8, 53, 40];
-
+    it.each([
+        [[2, 4, 21, -8, 53, 40], 6],
+        [[1, 2, 3, 4, 5], 5],
+        [[-10, -20, -30, -40, -50], 5],
+        [[0, 0, 0, 0, 0], 5],
+        [[100, 200, 300, 400, 500], 5],
+    ])('should know that the count of values in %s sequence is %s', (sequence, expected) => {
         const output = calculateStats(sequence);
 
-        expect(output.count).toBe(6)
+        expect(output.count).toBe(expected)
     })
 
     it('should know how to calculate the stats of an empty sequence', () => {
