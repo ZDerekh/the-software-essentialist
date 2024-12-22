518b982bc227d4ef8998238d253610f359aa982d
zderekh <zahariya.derekh@gmail.com>
2024-12-22 18:41:40 +0100

    feat: simplest verification by returning constant

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -1,5 +1,12 @@
+import validateMilitaryTime from './index';
 
 describe('military time validator', () => {
 
+    it.each([
+        ['01:12 - 14:32', true],
+    ])('confirm the %s time range is %s`thy military time', (timeRange, expected) => {
+        const output = validateMilitaryTime(timeRange);
 
+        expect(output).toBe(expected);
+    });
 })
---
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -0,0 +1,3 @@
+export default function validateMilitaryTime(timeRange: string): boolean {
+    return true;
+}
