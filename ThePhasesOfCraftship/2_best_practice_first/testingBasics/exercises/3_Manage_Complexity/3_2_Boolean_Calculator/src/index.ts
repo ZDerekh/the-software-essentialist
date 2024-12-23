f6b6674db90da5f8558590fb33636350487b5218
zderekh <zahariya.derekh@gmail.com>
2024-12-23 17:31:38 +0100

    refactor: flatten the logic

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
@@ -1,13 +1,17 @@
 
 function calculateSingleStatement(input: string): boolean {
-    const isReversed = input.startsWith('NOT');
-    const isTrue = input.includes('FALSE')!== true;
-    return isReversed ? !isTrue : isTrue;
+    return input.includes('FALSE')!== true;    
 }
 
 export default function booleanCalculator(input: string): boolean {
+    const hasNot = input.includes('NOT');
     const hasAnds = input.includes('AND');
     const hasOrs = input.includes('OR');
+
+    if (hasNot) {
+        const [_, right] = input.split('NOT');
+        return !calculateSingleStatement(right);
+    }
     if (hasAnds) {
         const [left, right] = input.split('AND');
         return calculateSingleStatement(left) && calculateSingleStatement(right);
@@ -17,6 +21,7 @@ export default function booleanCalculator(input: string): boolean {
         const [left, right] = input.split('OR');
         return calculateSingleStatement(left) || calculateSingleStatement(right);
     }
+
 
     return calculateSingleStatement(input);
 }
