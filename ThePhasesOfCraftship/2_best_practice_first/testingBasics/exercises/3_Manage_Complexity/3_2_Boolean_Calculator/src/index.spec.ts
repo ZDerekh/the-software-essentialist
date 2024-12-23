9bd0492fab4cee28606dd6d36c2cf47261d6ebcf
zderekh <zahariya.derekh@gmail.com>
2024-12-23 15:22:20 +0100

    refactor: splitted test to have not as a separate case

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -3,10 +3,17 @@ describe('boolean calculator', () => {
     it.each([
         ['TRUE', true],
         ['FALSE', false],
-        ['NOT TRUE', false],
-        ['NOT FALSE', true],
     ])('should know that %s resolves to %s', (input, expected) => {
         const result = booleanCalculator(input);
+        expect(result).toBe(expected);
+    });
+
+    it.each([
+        ['NOT TRUE', false],
+        ['NOT FALSE', true],
+    ])('should know how to handle NOT (%s) to resolve to %s', (input, expected) => {
+        const result = booleanCalculator(input);
+
         expect(result).toBe(expected);
     });
 });
