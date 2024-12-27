66192dc2450d7f1394d3c95199f7ec73058f81ab
zderekh <zahariya.derekh@gmail.com>
2024-12-27 23:45:03 +0100

    refactor: moved the normalization out of scope

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
@@ -19,47 +19,44 @@ function hasMultipleStatements(input: string): boolean {
     return input.includes(Operator.AND) || input.includes(Operator.OR);
 }
 
-
-  
-export default function booleanCalculator(input: string): boolean {
-    let trimmedInput = input.replace(/\s/g, '').trim();
-    const hasNot = trimmedInput.includes(Operator.NOT);
-    const hasAnds = trimmedInput.includes(Operator.AND);
-    const hasOrs = trimmedInput.includes(Operator.OR);
-    const hasParentheses = trimmedInput.includes('(');
+function evaluateBoolean(input: string): boolean {
+    const hasNot = input.includes(Operator.NOT);
+    const hasAnds = input.includes(Operator.AND);
+    const hasOrs = input.includes(Operator.OR);
+    const hasParentheses = input.includes('(');
 
 
     if (hasParentheses) {
-        const start = trimmedInput.indexOf('(');
-        const end = trimmedInput.lastIndexOf(')');
+        const start = input.indexOf('(');
+        const end = input.lastIndexOf(')');
 
-        const inner = trimmedInput.slice(start + 1, end);
-        const result = booleanCalculator(inner);
+        const inner = input.slice(start + 1, end);
+        const result = evaluateBoolean(inner);
 
      
-        trimmedInput = trimmedInput.replace(`(${inner})`, result ? Boolean.TRUE : Boolean.FALSE);
+        input = input.replace(`(${inner})`, result ? Boolean.TRUE : Boolean.FALSE);
         
-        return booleanCalculator(trimmedInput);
+        return evaluateBoolean(input);
     }
 
     if (hasNot) {
         
-        const conditions = trimmedInput.split(Operator.NOT);
+        const conditions = input.split(Operator.NOT);
 
         if (hasMultipleStatements(conditions[1])) {
-            return !booleanCalculator(conditions[1]);
+            return !evaluateBoolean(conditions[1]);
         }
         
         return !isTrue(conditions[1]);
     }
 
     if (hasAnds) {
-        const conditions = trimmedInput.split(Operator.AND);
+        const conditions = input.split(Operator.AND);
         let result = true;
 
         for(const condition of conditions) {
             if(hasMultipleStatements(condition)) {
-                return booleanCalculator(condition);
+                return evaluateBoolean(condition);
             }
 
             result = result && isTrue(condition);
@@ -69,12 +66,12 @@ export default function booleanCalculator(input: string): boolean {
     }
 
     if (hasOrs) {
-        const conditions = trimmedInput.split(Operator.OR);
+        const conditions = input.split(Operator.OR);
         let result = false;
 
         for(const condition of conditions) {
             if(hasMultipleStatements(condition)) {
-                return booleanCalculator(condition);
+                return evaluateBoolean(condition);
             }
 
             result = result || isTrue(condition);
@@ -83,5 +80,12 @@ export default function booleanCalculator(input: string): boolean {
         return result;
     }
 
-    return isTrue(trimmedInput);
+    return isTrue(input);
+}
+
+  
+export default function booleanCalculator(input: string): boolean {
+    let trimmedInput = input.replace(/\s/g, '').trim();
+   
+    return evaluateBoolean(trimmedInput);
 }
